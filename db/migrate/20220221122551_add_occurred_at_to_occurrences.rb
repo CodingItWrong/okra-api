7c1fafd47a4f841f1b1a1f44d907bb2c8f0781c9
Josh Justice <me@codingitwrong.com>
2022-02-21 07:39:05 -0500

    Fix manually-edited migration

--- db/migrate/20220221122551_add_occurred_at_to_occurrences.rb
+++ db/migrate/20220221122551_add_occurred_at_to_occurrences.rb
@@ -1,5 +1,5 @@
 class AddOccurredAtToOccurrences < ActiveRecord::Migration[7.0]
   def change
-    change_column :occurrences, :occurred_at, :datetime, null: false
+    add_column :occurrences, :occurred_at, :datetime, null: false
   end
 end
