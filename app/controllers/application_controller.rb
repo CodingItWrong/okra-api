03f3adff19496a4dc868ae1fda5e28816b008d4f
Josh Justice <me@codingitwrong.com>
2022-02-21 07:36:16 -0500

    Dunno:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # class ApplicationController < JSONAPI::ResourceController
 class ApplicationController < ActionController::API
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   private
 
