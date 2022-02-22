946868c506e7c46b78b857adc0afedbf485794f7
Josh Justice <me@codingitwrong.com>
2022-02-22 07:06:41 -0500

    Switch from base class to module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-class ApplicationController < JSONAPI::ResourceController
+class ApplicationController < ActionController::Base
+  include JSONAPI::ActsAsResourceController
   skip_before_action :verify_authenticity_token
 
   private
